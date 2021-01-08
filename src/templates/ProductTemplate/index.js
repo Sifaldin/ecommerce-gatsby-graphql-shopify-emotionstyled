89fb5dfc40fbc55cb04399a7c527c1c823164ed2
Sifaldin <saif.m.abbas@gmail.com>
2021-01-08 11:02:13 +0100

    start graphql query for collections

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -12,19 +12,7 @@ export const query = graphql`
     query productQuery($shopifyId: String){
       
         shopifyProduct(shopifyId: {eq: $shopifyId}){
-          shopifyId
-          title
-          description
-          images {
-            id
-            localFile {
-              childImageSharp {
-                fluid(maxWidth: 300) {
-                  ...GatsbyImageSharpFluid_withWebp
-                }
-              }
-            }
-          }
+         ...ShopifyProductFields
         }
     }
 `;
